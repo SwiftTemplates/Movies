b9eaae34d447f013fa3506b2a8fbae9f4ebd043b
Justin Bush <revblaze@users.noreply.github.com>
2021-06-26 13:06:10 -0400

    Import Bindings for Movie and asset states

---
+++ Movies/Movies/Bindings/MovieListState.swift
@@ -0,0 +1,38 @@
+//
+//  MovieListState.swift
+//  Movies
+//
+//  Created by Justin Bush on 2021-06-26.
+//
+
+import SwiftUI
+
+class MovieListState: ObservableObject {
+    
+    @Published var movies: [Movie]?
+    @Published var isLoading: Bool = false
+    @Published var error: NSError?
+
+    private let movieService: MovieService
+    
+    init(movieService: MovieService = MovieStore.shared) {
+        self.movieService = movieService
+    }
+    
+    func loadMovies(with endpoint: MovieListEndpoint) {
+        self.movies = nil
+        self.isLoading = true
+        self.movieService.fetchMovies(from: endpoint) { [weak self] (result) in
+            guard let self = self else { return }
+            self.isLoading = false
+            switch result {
+            case .success(let response):
+                self.movies = response.results
+                
+            case .failure(let error):
+                self.error = error as NSError
+            }
+        }
+    }
+    
+}

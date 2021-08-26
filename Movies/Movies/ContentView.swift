ca79cf61bfa1647bd164e9da6059a55721a0c8c0
Justin Bush <revblaze@users.noreply.github.com>
2021-08-26 12:06:35 -0400

    Add EnvironmentObject and MovieHome to ContentView

--- Movies/Movies/ContentView.swift
+++ Movies/Movies/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    MovieHome()
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+      .environmentObject(ModelData())
   }
 }
